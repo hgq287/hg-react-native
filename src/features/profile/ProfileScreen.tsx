aa81dd2ec7462d720b01358021db8b35305d9835
Hg Q <hgq287@gmail.com>
2025-07-12 16:09:05 +0700

    feat: implement a simple template for `ProfileScreen`

--- src/features/profile/ProfileScreen.tsx
+++ src/features/profile/ProfileScreen.tsx
@@ -1,47 +1,117 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/Feather';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import { useSelector } from 'react-redux';
 import { selectUser } from '@redux/slices/userSlice';
 
 import { colors, fonts } from '@styles';
 
+const profilePic = require('@assets/images/profile-pic.png');
 const ProfileScreen = () => {
   const user = useSelector(selectUser);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>User Profile</Text>
-      <Text style={styles.label}>Username:</Text>
-      <Text style={styles.value}>{user.username}</Text>
-      <Text style={styles.label}>Email:</Text>
-      <Text style={styles.value}>{user.email}</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.headerContainer}>
+
+      </View>
+
+      <Image
+        source={profilePic}
+        style={styles.profilePic}
+      />
+
+      <Text style={styles.name}>Hg Q.</Text>
+      <Text style={styles.username}>@hgq287</Text>
+      <Text style={styles.bio}>
+        Mobile App Solutions Architect & Open Source Enthusiast
+      </Text>
+
+      <Option icon={<Ionicons name="lock-closed-outline" size={20} />} label="Privacy" />
+      <Option icon={<Ionicons name="time-outline" size={20} />} label="Purchase History" />
+      <Option icon={<MaterialIcons name="help-outline" size={20} />} label="Help & Support" />
+      <Option icon={<Ionicons name="settings-outline" size={20} />} label="Settings" />
+    </ScrollView>
   );
 };
 
+const Option = ({ icon, label }) => (
+  <TouchableOpacity style={styles.option}>
+    <View style={styles.optionContent}>
+      {icon}
+      <Text style={styles.optionText}>{label}</Text>
+    </View>
+    <Icon name="chevron-right" size={20} color="#999" />
+  </TouchableOpacity>
+);
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: colors.white,
+    padding: 0,
+    alignItems: 'center',
   },
-  title: {
+  profilePic: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    borderWidth: 4,
+    borderColor: '#fff',
+    marginTop: 84,
+    marginBottom: 15,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+    elevation: 6,
+  },
+  socialIcons: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: '70%',
+    marginVertical: 15,
+  },
+  name: {
+    fontWeight: 'bold',
     fontSize: 24,
-    fontFamily: fonts.primaryBold,
-    color: colors.primary,
+  },
+  username: {
+    color: '#888',
+    marginBottom: 5,
+  },
+  bio: {
+    textAlign: 'center',
+    paddingHorizontal: 20,
+    color: '#444',
     marginBottom: 20,
   },
-  label: {
-    fontSize: 16,
-    fontFamily: fonts.primaryRegular,
-    color: colors.grayDark,
+  option: {
+    backgroundColor: '#eee',
+    padding: 15,
+    borderRadius: 15,
+    width: '100%',
+    marginVertical: 7,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
-  value: {
-    fontSize: 18,
-    fontFamily: fonts.primaryRegular,
-    color: colors.black,
-    marginBottom: 15,
+  optionContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+  },
+  optionText: {
+    marginLeft: 10,
+    fontSize: 16,
   },
 });
 
