3ae3ec587efe70235766f4dd5c54bca2dcf3c9d7
Hg Q <hgq287@gmail.com>
2026-01-03 15:56:12 +0700

    fix: validate alias paths during runtime error checking

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
 import { Provider } from 'react-redux';
 import React, { useRef } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, NavigationContainerRef } from '@react-navigation/native';
 
 import { store } from '@redux/store';
 import AppView from '@launch/AppView';
+
+type RootParamList = {
+  // Define your route params here
+};
 
 export default function App() {
   const navigationRef = useRef<NavigationContainerRef<RootParamList>>(null);
